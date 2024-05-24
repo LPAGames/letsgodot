6e9f0236de9f1e6a11c6bb2c370a51c98228e9ed
LPAGames <47326261+LPAGames@users.noreply.github.com>
2024-05-24 12:48:00 -0300

    dashboard

--- js/lg.js
+++ js/lg.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
     $('#navbar').load('htmls/navbar.html');
+    $('#initial-banner').load('htmls/initial-banner.html');
+    $('#dashboard').load('htmls/dashboard.html');
     $('#footer').load('htmls/footer.html');
 });
